cccf994f39eeb12fbb2127b3875f8ee8132a061d
Murilo B Alves <61016975+mbalves1@users.noreply.github.com>
2020-11-13 08:45:55 -0300

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -7,25 +7,37 @@ button.addEventListener('click', (event) => {
     let email = document.querySelector("#formGroupExampleInput2").value;
     let tel = document.querySelector("#formGroupExampleInput3").value;
     let tel2 = document.querySelector("#formGroupExampleInput4").value;
-    
-    
+    document.querySelector("#formGroupExampleInput").value = ""
+    document.querySelector("#formGroupExampleInput2").value =""
+    document.querySelector("#formGroupExampleInput3").value = ""
+    document.querySelector("#formGroupExampleInput4").value = ""
+
     let response = document.querySelector('#containerCandidate')
     response.innerHTML = '';
 
     let divName = document.createElement('div')
 
     let labelName = document.createElement('label')
+    labelName.classList.add('bold')
     let nameInclude = document.createElement('p')
 
-    let labelEmail = document.createElement('p')
+    let labelEmail = document.createElement('label')
+    labelEmail.classList.add('bold')
     let emailInclude = document.createElement('p')
 
+    let labelTel = document.createElement('label')
+    labelTel.classList.add('bold')
     let telInclude = document.createElement('p')
+
+    let labelTel2 = document.createElement('label')
+    labelTel2.classList.add('bold')
     let tel2Include = document.createElement('p')
    
 
     labelName.textContent = "Nome:"
     labelEmail.textContent = "E-mail:"
+    labelTel.textContent = "Telefone:"
+    labelTel2.textContent = "Telefone2:"
 
     nameInclude.textContent = name
     emailInclude.textContent = email
@@ -40,7 +52,14 @@ button.addEventListener('click', (event) => {
     divName.appendChild(telInclude)
     divName.appendChild(tel2Include)
    
-
+   
     
+
+    if(name == "" || email == "" || tel == ""){
+        alert("Preencha correto")
+    }else {
+        alert("concluido")
+    }
+
 })
       
